871d0a9403d0524c39da2045c41a3cbbbc789e2f
David Lavanchy <dplavanchy@gmail.com>
2021-07-06 19:15:02 -0400

    first commit

---
+++ KomodoCafe_ClassLibrary/MenuItems.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KomodoCafe_ClassLibrary
+{
+    public class MenuItems
+    {
+        public MenuItems(){}
+        public MenuItems(int mealNumber, string mealName, string mealDescription, List<string> mealIngredients, decimal mealPrice)
+        {
+            MealNumber = mealNumber;
+            MealName = mealName;
+            MealDescription = mealDescription;
+            MealIngredients = mealIngredients;
+            MealPrice = mealPrice;
+        }
+        public int MealNumber { get; set; }
+        public string MealName { get; set; }
+        public string MealDescription { get; set; }
+        public List<string> MealIngredients { get; set; }
+        public decimal MealPrice { get; set; }
+    }
+}
