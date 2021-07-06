61ff5f9e68c3207d868c4c41d0cfaab47865ffde
David Lavanchy <dplavanchy@gmail.com>
2021-07-06 19:27:22 -0400

    MenuItems repository methods

--- KomodoCafe_ClassLibrary/MenuItems_Repo.cs
+++ KomodoCafe_ClassLibrary/MenuItems_Repo.cs
@@ -8,5 +8,49 @@ namespace KomodoCafe_ClassLibrary
 {
     public class MenuItems_Repo
     {
+        List<MenuItems> _menuItemsList = new List<MenuItems>();
+
+        //create
+        public void CreateMenuItem(MenuItems menuItem)
+        {
+            _menuItemsList.Add(menuItem);
+        }
+
+        //read
+        public List<MenuItems> ReadMenuItems()
+        {
+            return _menuItemsList;
+        }
+
+        //delete
+        public bool DeleteItemFromList(int identificationNumber)
+        {
+            MenuItems menuItem = GetMenuItemByID(identificationNumber);
+
+            if(menuItem != null)
+            {
+                _menuItemsList.Remove(menuItem);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public MenuItems GetMenuItemByID(int identificationNumber)
+        {
+            foreach(MenuItems item in _menuItemsList)
+            {
+                if(identificationNumber == item.MealNumber)
+                {
+                    return item;
+                }
+
+            }
+                return null;
+            
+        }
     }
 }
