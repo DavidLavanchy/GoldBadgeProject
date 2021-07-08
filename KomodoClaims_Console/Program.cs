e3f8df6e6403519bc913a5bb0050b23292e2fde3
David Lavanchy <dplavanchy@gmail.com>
2021-07-08 19:19:25 -0400

    first KomodoClaims commit, class objects built

---
+++ KomodoClaims_Console/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KomodoClaims_Console
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            DateTime ClaimDate = new DateTime(04 / 03 / 2020);
+            DateTime IncidentDate = new DateTime(03 / 20 / 2020);
+
+            if ((ClaimDate <= IncidentDate) && (ClaimDate >= IncidentDate.Date.AddDays(-30))) 
+            {
+                Console.WriteLine("true"); 
+            }
+
+            else 
+            {
+                Console.WriteLine("false");
+            };
+            Console.ReadLine();
+
+        }
+        
+    }
+}
---
+++ KomodoClaims_POCOs/Claim.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KomodoClaims_POCOs
+{
+    public class Claim
+    {
+        public enum TypeOfClaim { Theft, Car, Home }
+        
+        public Claim(){}
+        public Claim(int claimID, TypeOfClaim claimType, string claimDescription, decimal claimAmount, DateTime incidentDate, DateTime claimDate)
+        {
+            ClaimID = claimID;
+            ClaimType = claimType;
+            ClaimDescription = claimDescription;
+            ClaimAmount = claimAmount;
+            IncidentDate = incidentDate;
+            ClaimDate = claimDate;
+        }
+
+        public int ClaimID { get; set; }
+        public TypeOfClaim ClaimType { get; set; }
+        public string ClaimDescription { get; set; }
+        public decimal ClaimAmount { get; set; }
+        public DateTime IncidentDate { get; set; }
+        public DateTime ClaimDate { get; set; }
+        public bool IsValid 
+        {
+            get
+            {
+                TimeSpan isValidTime = ClaimDate - IncidentDate;
+                if (isValidTime.TotalDays <= 30 && isValidTime.TotalDays >=0)
+                {
+                    return true;
+                }
+                else 
+                {
+                    return false;
+                };
+            }
+              
+        }
+    }
+}
