3907d29bfad6556c32f42e5ca06b0623b460a4b0
David Lavanchy <dplavanchy@gmail.com>
2021-07-16 14:30:12 -0400

    seed greetings and build out the view method of UI

--- KomodoGreetings_Console/ProgramUI.cs
+++ KomodoGreetings_Console/ProgramUI.cs
@@ -16,6 +16,7 @@ namespace KomodoGreetings_Console
 
         public void Run()
         {
+            SeedGreetings();
             Menu();
         }
 
@@ -74,7 +75,29 @@ namespace KomodoGreetings_Console
 
         private void ViewAllCustomers()
         {
-            throw new NotImplementedException();
+            List<Greetings> greetingsList = _repo.ReadListOfGreetings();
+
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"        {halfDashes} Komodo Greetings {halfDashes}");
+            Console.ResetColor();
+            Console.WriteLine("");
+            Console.WriteLine("                 All Greetings:\n" +
+                $" {dashes}{dashes}{dashes}\n");
+
+            Console.WriteLine("First Name:       Last Name:       Customer Type:       Email Message:");
+
+            foreach(var greeting in greetingsList)
+            {
+                Console.WriteLine($"{greeting.FirstName}        {greeting.LastName}     {greeting.TypeOfCustomer}       {greeting.Email}");
+                Console.WriteLine("");
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to return to the Main Menu");
+            Console.ReadKey();
+            Menu();
+
         }
 
         private void CreateACustomer()
@@ -90,6 +113,22 @@ namespace KomodoGreetings_Console
         private void DeleteACustomer()
         {
             throw new NotImplementedException();
+        }
+        public void SeedGreetings()
+        {
+            Greetings greeting1 = new Greetings("Scott", "Allen", Greetings.TypeOfGreeting.Potential, "Visit our website for a free quote today!");
+            Greetings greeting2 = new Greetings("Sarah", "Smith", Greetings.TypeOfGreeting.Current, "Visit our website to see if you are eligible for our loyalty discount.");
+            Greetings greeting3 = new Greetings("Caleb", "Wright", Greetings.TypeOfGreeting.Past, "We haven't heard from you in a while! Save 30% on your next purchase by using code KOMODO");
+            Greetings greeting4 = new Greetings("Allen", "McCallum", Greetings.TypeOfGreeting.Potential, "If you are in the market for car insurance, visit komodoclaims.com for a free quote today!");
+            Greetings greeting5 = new Greetings("Natalie", "Birch", Greetings.TypeOfGreeting.Past, "There are still items in your cart! Visit them here before their gone.");
+            Greetings greeting6 = new Greetings("Scottie", "Schefler", Greetings.TypeOfGreeting.Current, "You've qualified for a continuing customer discount for your next purchase!");
+
+            _repo.CreateANewGreeting(greeting1);
+            _repo.CreateANewGreeting(greeting2);
+            _repo.CreateANewGreeting(greeting3);
+            _repo.CreateANewGreeting(greeting4);
+            _repo.CreateANewGreeting(greeting5);
+            _repo.CreateANewGreeting(greeting6);
         }
     }
 
